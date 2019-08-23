13d3c855073297642e1d080903ea74e2886a6aec
Alexander Fountain <alexander@digett.com>
2019-08-23 17:14:42 -0500

    theme update archive link removed

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,10 +23,6 @@ module.exports={
                         name:'ENGAGE',
                         link:'/engage',
                      },
-                     {
-                        name:'ARCHIVE',
-                        link:'/archive',
-                     },
                 ],
                 colors:{
                     primary: '#1C3B61',

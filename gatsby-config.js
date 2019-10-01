77426629c8610142169c3a7092d9d066c2c88c74
Alexander Fountain <alexander@digett.com>
2019-10-01 11:39:49 -0500

    theme update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,47 +1,47 @@
-module.exports={
-    plugins: [
-        {
-            resolve: 'figdog-theme',
-            options: {
-                siteTitle: 'Figdog',
-                siteSlogan: 'PERSPECTIVES ON MARKETING AND MANAGED IT SERVICES',
-                logo:'/images/Logo-White.png',
-                logodark: `/images/Logo-Dark.png`,
-                topMenuLinks:[
-                    {
-                        name:'DARK MODE',
-                        link:'#',
-                        icon: 'fas fa-toggle-on'
-                    }
-                ],
-                footerMenuLinks:[
-                    {
-                        name:'ABOUT',
-                        link:'/about',
-                     },
-                     {
-                        name:'ENGAGE',
-                        link:'/engage',
-                     },
-                ],
-                colors:{
-                    primary: '#1C3B61',
-                    darkGray: "#424242",
-                    darkDarkGray: "#303030",
-                    lightGray: "#C4C4C4",
-                    cream: "#061836",
-                    gold: "#EBBA7D",
-                    orange: "#DE6936",
-                    red: "#BF0F0F",
-                    darkBlue: "#061836"
-
-                },
-                breakpoints:{
-                    mobileWidth:`611px`,
-                    tabletWidth:`611px`,
-                    desktopWidth:`612px`
-                }
-            }
+module.exports = {
+  plugins: [
+    {
+      resolve: "figdog-theme",
+      options: {
+        siteTitle: "Figdog",
+        siteSlogan: "PERSPECTIVES ON MARKETING AND MANAGED IT SERVICES",
+        title: `figdog`,
+        logo: "/images/Logo-White.png",
+        logodark: `/images/Logo-Dark.png`,
+        topMenuLinks: [
+          {
+            name: "DARK MODE",
+            link: "#",
+            icon: "fas fa-toggle-on"
+          }
+        ],
+        footerMenuLinks: [
+          {
+            name: "ABOUT",
+            link: "/about"
+          },
+          {
+            name: "ENGAGE",
+            link: "/engage"
+          }
+        ],
+        colors: {
+          primary: "#1C3B61",
+          darkGray: "#424242",
+          darkDarkGray: "#303030",
+          lightGray: "#C4C4C4",
+          cream: "#061836",
+          gold: "#EBBA7D",
+          orange: "#DE6936",
+          red: "#BF0F0F",
+          darkBlue: "#061836"
+        },
+        breakpoints: {
+          mobileWidth: `611px`,
+          tabletWidth: `611px`,
+          desktopWidth: `612px`
         }
-    ]
-}
+      }
+    }
+  ]
+};
